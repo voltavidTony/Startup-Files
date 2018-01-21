5851fb3602c41f79a0d4eb91dcb3309b95dbe01e
voltavidTony <anthonyplugins@gmail.com>
2018-01-21 14:04:07 -0500

    Changed type of commands
    Changed commands to window commands because application commands didn't work on startup

--- Startup Files.py
+++ Startup Files.py
@@ -4,17 +4,17 @@ import sublime_plugin
 
 # Run the command at startup
 def plugin_loaded():
-	sublime.run_command("open_startup_files")
+	sublime.active_window().run_command("open_startup_files")
 
 # Ensure all specified files are opened and assigned a tab
-class OpenStartupFilesCommand(sublime_plugin.ApplicationCommand):
+class OpenStartupFilesCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		setfiles = sublime.load_settings("Preferences.sublime-settings").get("open_files_at_startup", [])
 		openfiles = [view.file_name() for window in sublime.windows() for view in window.views()]
 		[sublime.active_window().open_file(file) for file in setfiles if not file in openfiles]
 
 # Set the list of files to all currently open files
-class SetStartupFilesCommand(sublime_plugin.ApplicationCommand):
+class SetStartupFilesCommand(sublime_plugin.WindowCommand):
 	def run(self):
 		sublime.load_settings("Preferences.sublime-settings").set("open_files_at_startup", \
 			[view.file_name() for window in sublime.windows() for view in window.views()])
